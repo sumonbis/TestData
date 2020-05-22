15f79f7b5c6ef2cec6aaeef2fe9a1276c66aca11
Sumon Biswas <sumon@iastate.edu>
2020-05-22 05:12:39 -0500

    Added for

--- test.py
+++ test.py
@@ -1,14 +1,1 @@
-try:
-	class MyClass:
-		"This is my second class"
-		a = 10
-		def func(self):
-			print('Hello')
-		
-		class Inner:
-		    """Inner Class"""
-
-		    def inner_display(self, msg):
-		        print(msg)
-except NameError:
-	print("Found error")
+print("Something")
