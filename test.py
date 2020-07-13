3ab3cb1b4b4fe15fd8062c776af21cb5f24a4f8c
Sumon Biswas <39540376+sumonbis@users.noreply.github.com>
2020-07-12 20:02:12 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,1 @@
-print("loooping----",loop_count)
-
-
-yt=[np.ones(30),np.ones(30)*2,np.ones(30)*3,np.ones(30)*4,np.ones(30)*5, \
-np.ones(30)*6,np.ones(30)*7,np.ones(30)*8,np.ones(30)*9,np.ones(30)*10]
+print(q, p, p * q, sep=",")
