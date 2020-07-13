43f9fef0565f9b4fc079ec95dd6fa16803cf7a81
Sumon Biswas <39540376+sumonbis@users.noreply.github.com>
2020-07-12 19:48:47 -0500

    a

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-class Msgrpc:
-    def __init__(self, option=[]):
-        self.host = option.get('host') or "127.0.0.1"
-        self.port = option.get('port') or 55552
+for batch in range(total_batch):
+            batch_x, batch_y = mnist.train.next_batch(batch_size)
+            sess.run(train_op, feed_dict={x:batch_x, y:batch_y})
+
+        if epoch % 10 == 0:
+            loss_, acc = sess.run([loss, accuracy], feed_dict={x:batch_x, y:batch_y})
+            print("epoch {},  loss {:.4f}, acc {:.3f}".format(epoch, loss_, acc))
